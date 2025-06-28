99e40f0f724bcf9b211b164e296ea62a70d4ae22
aspect19 <jeffamenya388@gmail.com>
2025-06-28 14:36:49 +0300

    Practice with composables

--- app/src/main/java/com/tuno/player/MainActivity.kt
+++ app/src/main/java/com/tuno/player/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.tuno.player.ui.theme.TunoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,42 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TunoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+
+
+                ) { innerPadding ->
+                    Column(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+
+                    ) {
+                        Greeting(
+                            name = "Jeff"
+                        )
+                    }
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TunoTheme {
-        Greeting("Android")
+fun Greeting(name: String) {
+    for (i in 1..4) {
+        Text(
+            modifier = Modifier.padding(3.dp),
+            text = "Hello $name and me!",
+        )
+        HorizontalDivider()
     }
+    Text(
+        modifier = Modifier.background(Color.Blue),
+        text = "The end"
+    )
 }
